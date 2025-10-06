ff0c51127d836b65ee8e3644d39e49f15729a9ab
Abhishek Yadav <stognvy@Mac.lan>
2025-10-06 21:32:25 +0530

    adding files

---
+++ src/main/java/com/yadav/website/firstProject/controller/helloController.java
@@ -0,0 +1,13 @@
+package com.yadav.website.firstProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @GetMapping("/")
+    public String helloContoller(){
+        return "HELLO WORLD!";
+    }
+}
